97771dadbd76c3f413e1ed590b539fae52efcee7
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-11-14 15:28:33 -0600

    (refactor) took weather and astronomy out of index and put into routes files

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const axios = require('axios');
+const bodyParser = require('body-parser');
+const { weather } = require('./routes/weather');
+const { astronomy } = require('./routes/astronomy');
 
 dotenv.config();
 const { PORT } = process.env;
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
+app.use('/weather', weather);
+
+app.use('/astronomy', astronomy);
 
 app.get('/', (req, res) => {
   res.status(200).send({
-    message: 'Hello!'
+    message: 'Please use /weather route to get the forecast. use /astronomy to get astronomical data',
   });
 });
